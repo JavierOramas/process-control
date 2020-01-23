fd3ed8bafb9a8ed6fcd2b94600d370a4f58c9110
Javier Oramas <javiale2000@gmail.com>
2020-01-23 13:53:08 -0500

    Show Data by day

--- dataframe.py
+++ dataframe.py
@@ -27,6 +27,18 @@ def split_date(dataframe):
     df['create_hour'] = [d.time() for d in df['create_time']]  
     return df
 
+def group_by_day(date,dateframe): 
+    month=date.month
+    year=date.year
+    day=date.day
+    
+    groups = dataframe.groupby(['create_year','create_month', 'create_day'])
+    return groups.get_group((str(year),str(month),str(day)))
+
+def group_by_hour(hour,dateframe): 
+    groups = dataframe.groupby(['create_year','create_month', 'create_day'])
+    return groups.get_group((str(year),str(month),str(day)))
+
 df_for_date=split_date(dataframe)
 #hour_data_group = dataframe.groupby(dataframe['create_time'].dt.hour)
 
@@ -34,10 +46,10 @@ df_for_date=split_date(dataframe)
 
 users = set(dataframe['username'])
 
-dataframe_by_name = pd.DataFrame(dataframe.groupby(['username']).sum(), columns=['memory_percent', 'cpu_percent','num_threads'])
+dataframe_by_name = pd.DataFrame(dataframe.groupby(['username']).sum(), columns=['memory_percent', 'cpu_percent','num_threads','create_day','create_month','create_day'])
 st.dataframe(dataframe_by_name)
 
-ndf = pd.DataFrame(dataframe.groupby(dataframe['create_time'].dt.day).mean(),columns=['memory_percent', 'cpu_percent','num_threads','cmdline'])
+ndf = pd.DataFrame(dataframe.groupby(dataframe['create_time'].dt.day).mean(),columns=['memory_percent', 'cpu_percent','num_threads','cmdline','create_day','create_month','create_day'])
 st.line_chart(ndf)
     
 info_mode = st.sidebar.radio('Ver info:',('General','Por Usuario'))
@@ -46,33 +58,48 @@ info_mode = st.sidebar.radio('Ver info:',('General','Por Usuario'))
 #day_data = day_data_group.get_group(date) 
 
 if info_mode == 'General':
-    details_mode = st.sidebar.checkbox('ver datos por hora',key='details')
     
-    if details_mode:
+    details_hour_mode = st.sidebar.checkbox('ver datos por hora',key='details')
+    if details_hour_mode:
+        date_hour = st.sidebar.date_input("fecha",datetime.datetime.now(), key="hourdate")
         hora = st.sidebar.slider("hora", 0,23,12,key="hora")
-        if hora in [x[0] for x in hour_data_group]:
-            hour_data = hour_data_group.get_group(hora)
-            hour_data
-            hour_df_data = pd.DataFrame(hour_data.groupby(hour_data['create_time'].dt.minute).sum(),columns=['memory_percent','cpu_percent','num_threads'])
-            hour_df_data
-            st.line_chart(hour_df_data)
-    # else:
-    #     day_data
-    #     day_df_data = pd.DataFrame(day_data.groupby(day_data[DATE_COLUMN].dt.minute).median(),
-    #                                 columns=['memory_percent', 'cpu_percent','num_threads','cmdline'])
-    #     day_df_data
+        # hour_data_group = df
+        # if hora in [x[0] for x in hour_data_group]:
+        #     hour_data = hour_data_group.get_group(hora)
+        #     hour_data
+        #     hour_df_data = pd.DataFrame(hour_data.groupby(hour_data['create_time'].dt.minute).sum(),columns=['memory_percent','cpu_percent','num_threads'])
+        #     hour_df_data
+        #     st.line_chart(hour_df_data)
+    
+    details_day_mode = st.sidebar.checkbox('ver datos de un día',key='details_day')
+    if details_day_mode:
+        date_day = st.sidebar.date_input("fecha",datetime.datetime.now(), key="daydate")
+        st.text(str(date_day.day)+'-'+str(date_day.month)+'-'+str(date_day.year))
+        try:            
+            df = group_by_day(date_day,ndf)  
+            df = pd.DataFrame(df.groupby('create_time').sum(),columns=['memory_percent','cpu_percent','num_threads'])
+            st.line_chart(df)
+        except:
+           st.text("No hay datos de la fecha selecionada") 
+        
 else:
     selected = st.multiselect("Usuarios", [x for x in users])
+    st.text(x for x in selected)
+
 
 summary = st.sidebar.checkbox("Mostrar total de recursos consumidos en un mes")
-
 if summary: 
-    date = st.sidebar.date_input("fecha",datetime.datetime.now())
+    date = st.sidebar.date_input("fecha",datetime.datetime.now(),key='summary_date')
     month=date.month
     year=date.year
-    st.text(year)
     tupla=(str(year),str(month))
     new_df=df_for_date.groupby(['create_year' , 'create_month'])
-    new_df=new_df.get_group(tupla)
-    new_df=new_df.drop(new_df.columns[6:], axis=1)
-    st.dataframe(new_df)
+    try:
+        new_df=new_df.get_group(tupla)
+        new_df=new_df.drop(new_df.columns[6:], axis=1)
+        st.dataframe(new_df)
+    except:
+        st.text("No hay datos de la fecha selecionada")
+        
+# def hour_filter(dataframe,date,):
+   
